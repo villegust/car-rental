0413924f7450c7422465f8cf8bac2e2ead1ea157
villegust <ville.gust@gmail.com>
2023-10-25 09:13:16 +0200

    Small change

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Route, Routes } from "react-router-dom";
 // import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
 import About from "./pages/About"
-import Models from "./pages/Models"
+import Fleet from "./pages/Fleet"
 import Team from "./pages/Team"
 import Contact from "./pages/Contact"
 import Reviews from "./pages/Reviews"
@@ -18,7 +18,7 @@ function App() {
       <Routes>
           <Route index path="/" element={ <Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/fleet" element={<Models />} />
+          <Route path="/fleet" element={<Fleet />} />
           <Route path="/team" element={<Team />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/reviews" element={<Reviews />} />
